c4c66a97f5b31704cb1c9adf971a4c2f92d70603
Ivan Perez <ivan.web9450@gmail.com>
2023-10-09 17:30:38 -0400

    server

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const http = require("http");
 const fs = require("fs");
 http
   .createServer((req, res) => {
-    fs.readFile("demofile.html", (err, data) => {
+    fs.readFile("index.html", (err, data) => {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
       res.end();
